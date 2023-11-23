6ff032c95b66df652e36f99f601d098e2e4c6e0e
oMFDOo <50069569+oMFDOo@users.noreply.github.com>
2023-11-23 14:59:51 +0900

    Update App.vue

--- enjoytrip7vue - 복사본/src/App.vue
+++ enjoytrip7vue - 복사본/src/App.vue
@@ -6,22 +6,22 @@ import { useAuthStore } from '@/store/authStore.js'
 
 const { authStore, logout } = useAuthStore()
 
-const loginStatus = sessionStorage.getItem("isLogin");
+const loginStatus = sessionStorage.getItem('isLogin')
 
-sessionStorage.setItem("isLogin", true);
+sessionStorage.setItem('isLogin', true)
 
-if(loginStatus){
-  authStore.isLogin = sessionStorage.getItem("isLogin");
-  authStore.name = sessionStorage.getItem("name");
-  authStore.profileImageUrl = sessionStorage.getItem("profileImageUrl");
+if (loginStatus) {
+  authStore.isLogin = sessionStorage.getItem('isLogin')
+  authStore.name = sessionStorage.getItem('name')
+  authStore.profileImageUrl = sessionStorage.getItem('profileImageUrl')
 
-    // 관리자일 경우 권한 설정을 위해
-    if(sessionStorage.getItem("role") == "ROLE_SUPERVISOR"){
-      authStore.isSupervisor = true;
-    }else if(sessionStorage.getItem("role") == "ROLE_MANAGER"){
-      authStore.isManager = true;
-    }
-}else{
+  // 관리자일 경우 권한 설정을 위해
+  if (sessionStorage.getItem('role') == 'ROLE_SUPERVISOR') {
+    authStore.isSupervisor = true
+  } else if (sessionStorage.getItem('role') == 'ROLE_MANAGER') {
+    authStore.isManager = true
+  }
+} else {
   logout()
 }
 </script>
@@ -31,10 +31,9 @@ if(loginStatus){
 
   <logo-bar></logo-bar>
 
-  <my-page></my-page>
+  <!-- <my-page></my-page> -->
 
   <router-view :key="$route.fullPath"></router-view>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
